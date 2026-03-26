70a4ef016cb0be3b42ea7933a6e2d62299f6cfb0
Tomcorian <tomek.chmura2004@gmail.com>
2026-03-26 11:16:54 +0100

    Added fps counter and saving tests

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import csv
+import time
+
+log_file = open('pomiary_gestow.csv', mode='w', newline='')
+log_writer = csv.writer(log_file)
+log_writer.writerow(['Timestamp', 'Gest_Zadany', 'Gest_Wykryty', 'FPS'])
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=2, detectionCon=0.7)
+pTime = 0
 
 print("Kamera uruchomiona. Wciśnij 'q', aby wyjść.")
+print("Naciśnij 's', aby zapisać wynik testu (np. gdy pokazujesz OTWARTĄ dłoń).")
 
 while True:
     success, img = cap.read()
@@ -37,16 +45,32 @@ while True:
                 elif fingers.count(1) >= 4:
                     gesture_text = "OTWARTA"
 
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    
+
     # --- RYSOWANIE MNIEJSZEGO INTERFEJSU ---
     # Dopasowane czarne tło i mniejsza czcionka (skala 1.0 zamiast 1.5)
     cv2.rectangle(img, (0, 0), (350, 60), (0, 0, 0), cv2.FILLED)
     cv2.putText(img, f'Gest: {gesture_text}', (15, 40),
                 cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv2.LINE_AA)
+    
+    cv2.putText(img, f'FPS: {int(fps)}', (500, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
     cv2.imshow("Touchless UI - Gestures", img)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
+    key = cv2.waitKey(1)
+    if key == ord('s'):
+        log_writer.writerow([time.time(), "OTWARTA", gesture_text, fps])
+        print(f"Zapisano pomiar: Wykryto {gesture_text}")
+
+    if key == ord('q'): break
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
+
+log_file.close()
 cap.release()
 cv2.destroyAllWindows()
